0c638f51d19d865e7c6a1ded70f657e5eade67ec
FlexBy420 <flexby420@gmail.com>
2025-02-17 22:16:46 +0100

    add PS4 json files, make some code improvements

--- tmdb-scraper.py
+++ tmdb-scraper.py
@@ -6,20 +6,23 @@ import logging
 import os
 from itertools import product
 
+os.makedirs("log", exist_ok=True)
+os.makedirs("xml", exist_ok=True)
+os.makedirs("json", exist_ok=True)
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-file_handler = logging.FileHandler('log.txt')
+log_file_path = os.path.join("log", "log.txt")
+file_handler = logging.FileHandler(log_file_path)
 file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 logging.getLogger().addHandler(file_handler)
 
-os.makedirs("xml", exist_ok=True)
-os.makedirs("json", exist_ok=True)
+FOUND_FILE = os.path.join("log", "found_links.txt")
 
 DOMAIN = "http://tmdb.np.dl.playstation.net/"
 SECRET_KEY = bytes.fromhex(
     "F5DE66D2680E255B2DF79E74F890EBF349262F618BCAE2A9ACCDEE5156CE8DF2CDF2D48C71173CDC2594465B87405D197CF1AED3B7E9671EEB56CA6753C2E6B0"
 )
-FOUND_FILE = "found_links.txt"
-MAX_CONCURRENT_REQUESTS = 20
+MAX_CONCURRENT_REQUESTS = 50
 
 # Generate HMAC hash
 def generate_hash(title_id):
@@ -36,17 +39,17 @@ async def fetch_tmdb(session, semaphore, title_id, path, extension):
                     file_path = f"{extension}/{title_id}.{extension}"
                     with open(file_path, "wb") as file:
                         file.write(await response.read())
-                    logging.info(f"Downloaded {title_id} to {file_path}")
+                    logging.info(f"Downloaded {title_id} to {file_path}") # Some XML and JSON files are just empty
                     with open(FOUND_FILE, "a") as log:
                         log.write(f"{title_id}: {url}\n")
         except Exception as e:
             logging.error(f"Error for {title_id}: {e}")
 
-# Scrape PSX, PS2, PS3 and PS4 IDs
+# Scrape PSX, PS2, PS3, and PS4 IDs
 async def scrape_all_ids():
     semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
-    ps3_digital = [f"NP{region}{rtype}" for region, rtype in product("AEHJKUIX", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")]
-    ps3_physical = [f"B{rights}{region}{rtype}" for rights, region, rtype in product("CL", "ACEHJKPU", "BCDMSTVXZ") if not (rtype == "M" and region != "J")]
+    ps3_digital = [f"NP{region}{rtype}" for region, rtype in product("EHJKU", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")] # A in region never been used it seems
+    ps3_physical = [f"B{rights}{region}{rtype}" for rights, region, rtype in product("CL", "ACEHJKU", "BCDMSX") if not (rtype == "M" and region != "J")]
     ps3_mrtc = [f"MRTC"]
     psx_ps2_physical = [f"S{rights}{region}{rtype}" for rights, region, rtype in product("CL", "ACEKPUZ", "ADJMNS")] # Other ones than these appear not be used? e.g. PAPX
     ps4_prefixes = [f"CUSA"]
@@ -54,9 +57,9 @@ async def scrape_all_ids():
 
     async with aiohttp.ClientSession() as session:
         for prefixes, path, ext in [
-            (ps3_digital + ps3_physical + psx_ps2_physical + ps3_mrtc, "tmdb", "xml"), # PSP and VITA may use this one too?
+            (psx_ps2_physical + ps3_physical + ps3_digital + ps3_mrtc, "tmdb", "xml"), # PSP and VITA may use this one too?
             (ps4_prefixes, "tmdb2", "json")
-            #(ps5_prefixes, "tmdb3", "json") # Appears to be a thing, cannot access it tho
+            #(ps5_prefixes, "tmdb3", "json") # Seems to be a thing, needs API key
         ]:
             for prefix in prefixes:
                 logging.info(f"Scraping {path.upper()} for prefix: {prefix}")
