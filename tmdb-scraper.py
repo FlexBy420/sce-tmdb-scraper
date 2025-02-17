e7f65d19d23d1fcfc49733146bcfb7c71a6fa20c
FlexBy420 <flexby420@gmail.com>
2025-02-18 00:31:45 +0100

    add BCAS xmls

--- tmdb-scraper.py
+++ tmdb-scraper.py
@@ -22,13 +22,13 @@ DOMAIN = "http://tmdb.np.dl.playstation.net/"
 SECRET_KEY = bytes.fromhex(
     "F5DE66D2680E255B2DF79E74F890EBF349262F618BCAE2A9ACCDEE5156CE8DF2CDF2D48C71173CDC2594465B87405D197CF1AED3B7E9671EEB56CA6753C2E6B0"
 )
-MAX_CONCURRENT_REQUESTS = 50
+MAX_CONCURRENT_REQUESTS = 2000
 
 # Generate HMAC hash
 def generate_hash(title_id):
     return hmac.new(SECRET_KEY, f"{title_id.upper()}_00".encode(), hashlib.sha1).hexdigest().upper()
 
-# Fetch and download TMDb data
+# Fetch and download TMDB data
 async def fetch_tmdb(session, semaphore, title_id, path, extension):
     async with semaphore:
         url = f"{DOMAIN}{path}/{title_id}_00_{generate_hash(title_id)}/{title_id}_00.{extension}"
@@ -49,7 +49,7 @@ async def fetch_tmdb(session, semaphore, title_id, path, extension):
 async def scrape_all_ids():
     semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
     ps3_digital = [f"NP{region}{rtype}" for region, rtype in product("EHJKU", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")] # A in region never been used it seems
-    ps3_physical = [f"B{rights}{region}{rtype}" for rights, region, rtype in product("CL", "ACEHJKU", "BCDMSX") if not (rtype == "M" and region != "J")]
+    ps3_physical = [f"B{rights}{region}{rtype}" for rights, region, rtype in product("CL", "AEJKU", "BCDMSX") if not ((rtype in ["M", "B"]) and region != "J")] # C, H in region never been used it seems
     ps3_mrtc = [f"MRTC"]
     psx_ps2_physical = [f"S{rights}{region}{rtype}" for rights, region, rtype in product("CL", "ACEKPUZ", "ADJMNS")] # Other ones than these appear not be used? e.g. PAPX
     ps4_prefixes = [f"CUSA"]
@@ -57,7 +57,7 @@ async def scrape_all_ids():
 
     async with aiohttp.ClientSession() as session:
         for prefixes, path, ext in [
-            (psx_ps2_physical + ps3_physical + ps3_digital + ps3_mrtc, "tmdb", "xml"), # PSP and VITA may use this one too?
+            (psx_ps2_physical + ps3_physical + ps3_mrtc + ps3_digital, "tmdb", "xml"), # PSP and VITA may use this one too?
             (ps4_prefixes, "tmdb2", "json")
             #(ps5_prefixes, "tmdb3", "json") # Seems to be a thing, needs API key
         ]:
