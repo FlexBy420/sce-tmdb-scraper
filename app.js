b1349e8830c17f0a39998aec636b1a35af82f505
FlexBy420 <flexby420@gmail.com>
2025-03-18 18:08:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -179,6 +179,20 @@ function filterGames() {
     });
 }
 
+// To top button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+}
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 document.addEventListener('DOMContentLoaded', init);
 
 async function init() {
